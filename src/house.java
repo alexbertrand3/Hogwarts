245f170f0c9c853c9a61f19a5452a9c73d4be73c
heliumballoons <60549755+heliumballoons@users.noreply.github.com>
2020-02-01 18:00:57 -0500

    asdf

---
+++ src/house.java
@@ -0,0 +1,17 @@
+
+import java.util.Scanner;
+
+public class house {
+	
+	  public static void main(String[] args) {
+	    	
+	  Scanner input = new Scanner(System.in);
+	  
+	  System.out.print("Enter an integer: ");
+  		int number = input.nextInt();
+  		System.out.println("You entered " + number);
+	  }	    
+	   
+	}
+	
+	
